08f6d9a2d868d947e2ede13c421b6d0a264252fa
Konrad <uriootv@protonmail.com>
2021-09-20 21:27:00 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use rand::Rng;
+use file_indexing;
+use scanrs::scann;
+fn main() {
+    println!("How many people do you want to gen?\n");
+    let peopletimes: usize = scann();
+
+    let namefile = "D:\\Programowansko\\namenpesel-gen\\src\\names.txt";
+    let lastnamefile = "D:\\Programowansko\\namenpesel-gen\\src\\lastnames.txt";
+    let writefile = "D:\\Programowansko\\namenpesel-gen\\src\\namenpesel.txt";
+
+    fs::write(writefile, "").expect("File clean failure");
+    let firstnames = fs::read_to_string(namefile)
+                .expect("Something went wrong reading the file");
+    let lastnames = fs::read_to_string(lastnamefile)
+                .expect("Something went wrong reading the file");
+
+
+    let mut firstnamelines: Vec<String> = firstnames.split_whitespace().map(|s| s.to_string()).collect();
+    let mut lastnamelines: Vec<String> = lastnames.split_whitespace().map(|s| s.to_string()).collect();
+
+    let line = connect_pesel(&mut firstnamelines, &mut lastnamelines, peopletimes);
+        //fs::write("D:\\Programowansko\\namenpesel-gen\\src\\namenpesel.txt",line)
+        //            .expect("Something went wrong writing the file");
+        
+    file_indexing::pust(writefile, line.as_str()).expect("Something went wrong writing the file");
+}
+
+fn connect_pesel(names: &mut Vec<String>, lastnames: &mut Vec<String>, times: usize) -> String{
+    let mut rng = rand::thread_rng();
+    let mut namenpesel = "".to_string();
+    let mut showing: String;
+    for i in 0..times{
+        showing = format!("{} {} {}\n", names[rng.gen_range(0..names.len())],lastnames[rng.gen_range(0..lastnames.len())] , rng.gen_range::<u64, _>(10000000000..99999999999));
+        namenpesel += showing.as_str();
+        println!("{}/{}",i ,times);
+        
+    }
+    return namenpesel;
+}
